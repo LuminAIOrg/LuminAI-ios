cdb190f120127599af215e96ffda8d2cfe32d8c2
ManuelPuchner <75803474+ManuelPuchner@users.noreply.github.com>
2024-02-08 15:22:37 +0100

    init

---
+++ LuminAI-ios/LuminAI_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuminAI_iosApp.swift
+//  LuminAI-ios
+//
+//  Created by Manuel Puchner on 08.02.24.
+//
+
+import SwiftUI
+
+@main
+struct LuminAI_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
